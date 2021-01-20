be1fb82828b768a02ed15f741dd617652800e61b
Jay McGrath <jaymcgrath@users.noreply.github.com>
2021-01-20 11:59:29 -0800

    (feat) wip add muting rule schedule fields

--- newrelic/resource_newrelic_alert_muting_rule.go
+++ newrelic/resource_newrelic_alert_muting_rule.go
@@ -1,12 +1,64 @@
 package newrelic
 
 import (
-	"log"
-
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/newrelic/newrelic-client-go/pkg/errors"
+	"log"
 )
+
+func scheduleSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"end_repeat": {
+				Type: schema.TypeString,
+				Optional: true,
+				Description: "The datetime stamp when the MutingRule schedule should stop repeating.",
+				//TODO: add validation func
+			},
+			"end_time": {
+				Type: schema.TypeString,
+				Optional: true,
+				Description: "The datetime stamp representing when the MutingRule should end.",
+				//TODO: add validation func
+			},
+			"repeat": {
+				//TODO: should this be an enum type? Should we mention enum values in desc?
+				Type: schema.TypeString,
+				Optional: true,
+				Description: "The frequency the MutingRule schedule repeats.",
+				//TODO: add validation func
+			},
+			"repeat_count": {
+				Type: schema.TypeInt,
+				Optional: true,
+				Description: "The number of times the MutingRule schedule should repeat.",
+				//TODO: add validation func?
+			},
+			"start_time": {
+				Type: schema.TypeString,
+				Optional: true,
+				Description: "The datetime stamp representing when the MutingRule should start.",
+				//TODO: add validation func
+			},
+			"time_zone": {
+				Type: schema.TypeString,
+				Required: true,
+				Description: "The time zone that applies to the MutingRule schedule.",
+				//TODO: add validation func
+			},
+			"weekly_repeat_days": {
+				Type: schema.TypeString,
+				Optional: true,
+				Description: "The day(s) of the week that a MutingRule should repeat when the repeat field is set to WEEKLY.",
+				//TODO: Change to Type: schema.TypeList,
+				//MinItems: 1,
+				//MaxItems: 7,
+				//Elem:
+			},
+		},
+	}
+}
 
 func resourceNewRelicAlertMutingRule() *schema.Resource {
 	return &schema.Resource{
@@ -81,6 +133,14 @@ func resourceNewRelicAlertMutingRule() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Description: "The description of the MutingRule.",
+			},
+			"schedule": {
+				Type:          schema.TypeList,
+				MinItems:      1,
+				MaxItems:      1,
+				Optional:      true,
+				Elem:          scheduleSchema(),
+				Description:   "The time window when the MutingRule should actively mute violations.",
 			},
 		},
 	}
--- newrelic/resource_newrelic_alert_muting_rule_test.go
+++ newrelic/resource_newrelic_alert_muting_rule_test.go
@@ -44,6 +44,38 @@ func TestAccNewRelicAlertMutingRule_Basic(t *testing.T) {
 	})
 }
 
+func TestAccNewRelicAlertMutingRule_WithSchedule(t *testing.T) {
+	resourceName := "newrelic_alert_muting_rule.foo"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		// CheckDestroy: testAccCheckNewRelicAlertMutingRuleDestroy,
+		Steps: []resource.TestStep{
+			// Test: Create
+			{
+				Config: testAccNewRelicAlertMutingRuleWithSchedule(rName, "new muting rule", "product", "EQUALS", "APM"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicAlertMutingRuleExists(resourceName),
+				),
+			},
+			// Test: Update
+			{
+				Config: testAccNewRelicAlertMutingRuleWithSchedule(rName, "second muting rule", "conditionType", "NOT_EQUALS", "baseline"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicAlertMutingRuleExists(resourceName),
+				),
+			},
+			// // Test: Import
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true},
+		},
+	})
+}
+
 func testAccNewRelicAlertMutingRuleBasic(
 	name string,
 	description string,
@@ -72,6 +104,43 @@ resource "newrelic_alert_muting_rule" "foo" {
 	}
 }
 `, name, description, attribute, operator, values)
+}
+
+func testAccNewRelicAlertMutingRuleWithSchedule(
+	name string,
+	description string,
+	attribute string,
+	operator string,
+	values string,
+) string {
+	return fmt.Sprintf(`
+
+resource "newrelic_alert_muting_rule" "foo" {
+	name = "tf-test-%[1]s"
+	enabled = true
+	description = "%[2]s"
+	condition {
+		conditions {
+			attribute 	= "%[3]s"
+			operator 	= "EQUALS"
+			values 		= ["%[5]s"]
+		}
+		conditions {
+			attribute 	= "conditionType"
+			operator 	= "%[4]s"
+			values 		= ["static"]
+		}
+		operator = "AND"
+	}
+	schedule {
+		start_time         = "2021-01-21T15:30:00"
+		end_time           = "2021-01-21T16:30:00"
+		time_zone          = "America/Los_Angeles"
+		repeat             = "WEEKLY"
+		end_repeat         = "2021-06-11T12:00:00"
+		weekly_repeat_days = "['MONDAY', 'TUESDAY', 'FRIDAY']"
+	}
+}`, name, description, attribute, operator, values)
 }
 
 func testAccCheckNewRelicAlertMutingRuleExists(n string) resource.TestCheckFunc {
